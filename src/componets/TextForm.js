5f3995abfbc51de98dd803f3b1f646d63a5d8487
Sarvam <sarvampatel456@gmail.com>
2025-06-09 12:12:07 +0530

    Textform update 2

--- src/componets/TextForm.js
+++ src/componets/TextForm.js
@@ -1,17 +1,25 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
+    const handleUpClick = () => {
+        console.log("Uppercase was clicked");
+        setText("You have clicked on handleUpClick");
+    }
+    const handleOnChange = (event) => {
+        console.log("On Change");
+        setText(event.target.value);
+    }
     const [text, setText] = useState('Enter text here');
     // text = "new text"; wrong way to change text
-    // setText = ("new tetx"); correct way to chnage state
+    // setText("new tetx"); correct way to chnage state
   return (
     <div>
       <form>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={text} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>        
-        <button className="btn btn-primary">Convert to Uppercase</button>
+        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
         </form>
     </div>
   )
