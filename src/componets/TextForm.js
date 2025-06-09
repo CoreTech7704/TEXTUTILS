8c6072cc141205ffdd01ff0f6f3111524cd12f9c
Sarvam <sarvampatel456@gmail.com>
2025-06-09 12:03:08 +0530

    Textform update 1

--- src/componets/TextForm.js
+++ src/componets/TextForm.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function TextForm(props) {
+    const [text, setText] = useState('Enter text here');
+    // text = "new text"; wrong way to change text
+    // setText = ("new tetx"); correct way to chnage state
   return (
     <div>
       <form>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} id="myBox" rows="8"></textarea>
         </div>        
         <button className="btn btn-primary">Convert to Uppercase</button>
         </form>
